846fb501eb115ade9e70ca666cdaf532794032ee
Kai <frenk.el.kai@gmail.com>
2021-12-01 09:13:45 +0100

    commit to merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 import {useState} from "react";
 
 function Footer (content) {
-    const [a, setA] = useState(content.items[2].fields.helloIAmSomeText)
-    console.log(a)
+    //const [a, setA] = useState(content.items[2]?.fields.helloIAmSomeText)
+    //console.log(a)
 
     return <div>hello I am some footer</div>
 
