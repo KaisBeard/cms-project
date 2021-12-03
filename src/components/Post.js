9d74c10926e40190cf03be1e194e907c3ccf7c37
Jaisree Ramesh <jaisree.ramesh@gmail.com>
2021-12-03 14:47:01 +0100

    Added dummy ad page and a edited home page

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,17 +5,16 @@ import './post.css'
 
 function Post({ article }) {
 
-    const[isOpen, setIsOpen] = useState(true);
     const {name, image, linkRef,description} = article.fields
     console.log(article.fields);
     return(
         <div className='post'>
             {image && <img className='image' src={image.fields.file.url} alt={name} title={name} /> }
             <div className='title'>
-            <button  id= 'collapsebtn' onClick={() => setIsOpen(!isOpen) }> {name} </button>
+            <button  id= 'collapsebtn' > {name} </button>
             <button id='linksbtn' onClick={linkRef}> MORE </button>
             </div>
-            <div className={`description ${isOpen ? 'collapsed' : 'expanded'}`} >{description}</div>
+            <div className="description">{description}</div>
             
         </div>
     )
